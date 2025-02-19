70c062e74e58f037ed0203a25ae82df46b942eda
Jika <vandechat@tutanota.com>
2025-02-19 23:13:02 +0100

    Use devname() to find correct freebsd pts

--- src/process/freebsd.cpp
+++ src/process/freebsd.cpp
@@ -23,24 +23,23 @@
 #include <sys/socket.h>
 #include <sys/stat.h>
 #include <sys/sysctl.h>
+#include <sys/types.h>
 #include <sys/user.h>
 #include <libprocstat.h>
-#include <sys/types.h>
 
 constexpr auto max_size = std::numeric_limits<std::streamsize>::max();
 
 Process::Process(int pid)
     : pid(pid)
 {
-    auto* procstat = procstat_open_sysctl();
+    auto *procstat = procstat_open_sysctl();
     unsigned int count;
-    auto* proc = procstat_getprocs(procstat, KERN_PROC_PID, this->pid, &count);
+    auto *proc = procstat_getprocs(procstat, KERN_PROC_PID, this->pid, &count);
     state = proc->ki_stat;
     ppid = proc->ki_ppid;
     process_group_id = proc->ki_pgid;
     session_id = proc->ki_tsid;
     tty_nr = proc->ki_tdev;
-
-    minor_dev =  (tty_nr & ((0b111) << 9))>>3 | (tty_nr & 0b11111);
-    pty_path = fmt::format("/dev/pts/{}", minor_dev);
+    minor_dev = (tty_nr & ((0b11) << 9)) >> 3 | (tty_nr & 0b11111);
+    pty_path = fmt::format("/dev/{}", devname(tty_nr, S_IFCHR));
 }
